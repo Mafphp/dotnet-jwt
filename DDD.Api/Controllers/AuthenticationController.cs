1b4983cc46fbdcd51d1250056fa33caf6f6bb854
mmdiq <Mafphp@gmail.com>
2023-10-10 14:11:21 +0330

    ADD: auth controller

--- DDD.Api/Controllers/AuthenticationController.cs
+++ DDD.Api/Controllers/AuthenticationController.cs
@@ -1,10 +1,25 @@
+using DDD.Contracts.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DDD.Api.Controllers;
 
-public class AuthenticationController : ControllerBase {
+[ApiController]
+[Route("auth")]
+public class AuthenticationController : ControllerBase
+{
     public AuthenticationController()
     {
-        
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        return Ok(request);
     }
 }
